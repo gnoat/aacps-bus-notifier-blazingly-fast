3a78b1c3f4e0b52a2f9d529dfed092e659b63b04
shane stephenson (shalst) <stephenson.shane.a@gmail.com>
2022-11-14 00:00:58 -0500

    fixed bug with new schedule comparison

--- crates/schecker/src/main.rs
+++ crates/schecker/src/main.rs
@@ -1,8 +1,15 @@
 use schecker::configs::Configs;
 use schecker::schedule;
+use std::{thread, time};
 
 fn main() {
     let configs: Configs = Configs::new("defaults.toml");
     let bus_url = schedule::BusInfoWebsite::Url(configs.schedule_url);
-    let _bus_info = schedule::BusInfo::new(bus_url);
+    let bus_info = schedule::BusInfo::new(bus_url);
+    loop {
+        let bus_info = bus_info.update(None);
+        let bus_diff = bus_info.diff();
+        println!("New: {:?}\n\nUpdated: {:?}\n\nRemoved: {:?}\n", bus_diff.new, bus_diff.updated, bus_diff.now_running);
+        thread::sleep(time::Duration::from_secs(5));
+    }
 }
